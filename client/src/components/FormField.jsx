c7e7d43a2d8a51908db53b4d4b3e65cb69c39735
Soham Samal <sohamsamal37@gmail.com>
2024-02-25 23:14:55 +0530

    Completing create-post frontend

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
-    <div>FormField</div>
+    <div>
+      <div className={`flex items-center gap-2 mb-2 
+      ${isSurpriseMe && 'justify-between'}`}>
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium text-gray-900'
+        >
+        {LabelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type = "button"
+            onClick={handleSurpriseMe}
+            className='inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input 
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      />
+    </div>
   )
 }
 
