c65cc7550674cd1d1b595b957d385acaca100d49
Soham Samal <sohamsamal37@gmail.com>
2024-02-26 01:58:44 +0530

    Finalising backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js'
+import someRoutes from './routes/someRoutes.js'
 
 const port = 5173;
 
@@ -11,6 +13,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/some', someRoutes);
+
 
 app.get('/', async (req, res) => {
     res.send('Hello from SOM-E');
